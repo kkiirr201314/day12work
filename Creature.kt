5440f7bbe5b8c2508dee7d6ec56f7ddbce450870
“kkiirr201314” <qq255073@gmail.com>
2021-07-01 22:11:46 +0800

    c16-4

--- Creature.kt
+++ Creature.kt
@@ -1,8 +1,13 @@
+import kotlin.random.Random
+
 interface Fightable {
     var healthPoints: Int
     val diceCount: Int
     val diceSides: Int
     val damageRoll: Int
+        get() = (0 until diceCount).map{
+            Random().nextInt(diceSides) + 1
+        }.sum()
 
     fun attack(opponent: Fightable): Int
 }
